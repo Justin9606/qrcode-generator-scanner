c5667d5c95c2f6a8aefa4b2178174785bab6f996
Justin <tonybek.96@gmail.com>
2021-07-13 17:46:40 +0900

    added yarn

--- src/useLocalStorage.js
+++ src/useLocalStorage.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const PREFIX = "codepen-clone-";
+const PREFIX = "qrCode-scan-";
 
 export default function useLocalStorage(key, initialValue) {
   const prefixedKey = PREFIX + key;
